cdd80255fe3a6ca2297f1ff936ddb570296c2026
Dylan Steck <dylancsteck@gmail.com>
2020-05-23 18:29:08 -0400

    Main desktop and mobile home layout done

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -27,17 +27,18 @@ const Portfolio = () => (
             const index = data.gcms.portfolioPieces.indexOf(piece)
             const isOdd = index % 2
             const { title, subtitle, featureImage, year, linkType, linkRef } = piece
+            //figure out if you need a grid or not
             return (
-              <div className="portfolio-piece" style={{width: '55vw', height: '35vh', marginBottom: '70vh', float: isOdd ? 'right' : 'left' }}>
+              <div className="portfolio-piece" style={{width: '55vw', height: '35vh', marginBottom: '75vh', float: isOdd ? 'right' : 'left'}}>
                 <div style={{textAlign: isOdd ? 'right' : 'left' }}>
-                  <h3 className="title" style={{fontSize: '175%', color: '#545FAB'}}>{title}</h3>
+                  <h3 className="title" style={{fontSize: '200%', color: '#545FAB'}}>{title}</h3>
                   <p className="subtitle" style={{fontSize: '150%', color: '#E2E2E2'}}>{year}</p>
                 </div>
                 <img className="feature-img" src={featureImage.url} style={{width: '50vw', float: isOdd ? 'right' : 'left' , border: '15px solid #545FAB', zIndex: 1}}/>
-                <div style={{backgroundColor: '#545FAB', width: '13vw', height: '13vw', float: isOdd ? 'left' : 'right' , color: '#FDF9FD', position: 'relative', bottom: '30%', zIndex: -1}}>
-                  <div style={{position: 'relative', top: '50%', padding: '2.5%'}}>
-                    <p>{subtitle}</p>
-                    <a href={linkRef} className="link" style={{color: '#FDF9FD', textDecoration: 'none'}}>{linkType}</a>
+                <div className="portfolio-piece-block" style={{backgroundColor: '#545FAB', width: '10vw', height: '10vw', float: isOdd ? 'left' : 'right' , color: '#FDF9FD', position: 'relative', bottom: '25%', right: isOdd ? '2.5%' : '-2.5%', zIndex: -1, textAlign: 'left', padding: '5%'}}>
+                  <div className="portfolio-piece-block-text" style={{marginTop: '50%', zIndex: 1}}>
+                    <p style={{fontWeight: 300, fontSize: '1.3vw'}}>{subtitle}</p>
+                    <a href={linkRef} className="link" style={{color: '#FDF9FD', textDecoration: 'none', fontWeight: 600, fontSize: '.85vw'}}>{linkType}</a>
                   </div>
                 </div>
               </div>
