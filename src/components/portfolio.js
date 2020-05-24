4930527886fe02ff7feb36375c488fef0b18b258
Dylan Steck <dylancsteck@gmail.com>
2020-05-24 13:33:47 -0400

    Main portfolio responsive and done
    Now time for footer, about, contact, and slight animations

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -20,23 +20,23 @@ const Portfolio = () => (
       }
     `}
     render={data => (
-      <div style={{marginTop: '65vh'}}>
-        <h1 style={{fontSize: '5vw'}}>Take a look at what I've built: </h1>
-        <div className="portfolio-grid">
+      <div className="portfolio" style={{marginTop: '65vh'}}>
+        <h1 style={{fontSize: '6.5vw', marginBottom: '10vh'}}>Take a look at what I've built: </h1>
+        <div className="portfolio-grid" style={{margin: '2.5%'}}>
           {data.gcms.portfolioPieces.map(piece => {
             const index = data.gcms.portfolioPieces.indexOf(piece)
-            const isOdd = index % 2
+            const isEven = !(index % 2)
             const { title, subtitle, featureImage, year, linkType, linkRef } = piece
             //figure out if you need a grid or not
             return (
-              <div className="portfolio-piece" style={{width: '75vw', height: '40vh', marginBottom: '100vh', float: isOdd ? 'right' : 'left', display: 'grid', gridTemplateColumns: 'auto', gridTemplateRows: 'auto'}}>
-                <div style={{textAlign: isOdd ? 'right' : 'left' }}>
-                  <h3 className="title" style={{fontSize: '200%', color: '#545FAB', whiteSpace: 'nowrap', float: 'left'}}>{title}</h3>
-                  <p className="subtitle" style={{fontSize: '150%', color: '#E2E2E2', float: 'left'}}>{year}</p>
+              <div className="portfolio-piece" style={{position: 'relative', width: '60vw', height: '40vh', marginBottom: '100vh', float: isEven ? 'left' : 'right'}}>
+                <div style={{textAlign: isEven ? 'left' : 'right', marginBottom: '15%'}}>
+                  <h3 className="title" style={{fontSize: '200%', color: '#545FAB'}}>{title}</h3>
+                  <p className="subtitle" style={{fontSize: '150%', color: '#E2E2E2'}}>{year}</p>
                 </div>
-                <img className="feature-img" src={featureImage.url} style={{width: '65vw', float: isOdd ? 'right' : 'left' , border: '20px solid #545FAB', zIndex: 1, gridColumn: isOdd ? '2 / span 7' : '3 / span 8', gridRow: '2 / span 8'}}/>
-                <div className="portfolio-piece-block" style={{backgroundColor: '#545FAB', width: '12.5vw', height: '12.5vw', float: isOdd ? 'left' : 'right' , color: '#FDF9FD', zIndex: -1, textAlign: 'left', padding: '5%', gridColumn: isOdd ? '1 / span 3' : '7 / span 9', gridRow: '9 / span 10'}}>
-                  <div className="portfolio-piece-block-text" style={{marginTop: '45%', zIndex: 1}}>
+                <div style={{background: `url(${featureImage.url}) no-repeat center center`, backgroundSize: 'cover',  width: '65vw', height: '50vh', float: isEven ? 'left' : 'right', border: '16px solid #545FAB', zIndex: 1, position: 'absolute', top: '50%', marginTop: '20%'}}></div>
+                <div className="portfolio-piece-block" style={{backgroundColor: '#545FAB', width: '12.5vw', height: '12.5vw', float: isEven ? 'right' : 'left' , color: '#FDF9FD', zIndex: -1, padding: '5%', position: 'absolute', top: '195%', right: isEven ? '-25%' : '75%'}}>
+                  <div className="portfolio-piece-block-text" style={{marginTop: '70%', zIndex: 1}}>
                     <p style={{fontWeight: 300, fontSize: '1.2vw'}}>{subtitle}</p>
                     <a href={linkRef} className="link" style={{color: '#FDF9FD', textDecoration: 'none', fontWeight: 600, fontSize: '.85vw'}}>{linkType}</a>
                   </div>
